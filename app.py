e9abdcc8de98f407d0b14744e6823d526d1c3bba
prasanth2020501099 <chaluvadikumar97@msitprogram.net>
2022-03-04 15:50:25 +0530

    update app.py
    training corpus telugu language also added

--- app.py
+++ app.py
@@ -46,15 +46,17 @@ english_bot = ChatBot(
     'Bot',logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch'}],
     )
 
-# english_bot.set_trainer(ListTrainer)
+english_bot.set_trainer(ListTrainer)
 
 
-# for file in os.listdir('data'):
-#     convData = open('data/' + file).readlines()
-#     english_bot.train(convData)
+for file in os.listdir('data'):
+    convData = open('data/' + file).readlines()
+    english_bot.train(convData)
 
-trainer=ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
+english_bot.set_trainer(ChatterBotCorpusTrainer)
+
+english_bot.train("chatterbot.corpus.english")
+english_bot.train("chatterbot.corpus.telugu")
 
 @app.route('/')
 def login():
